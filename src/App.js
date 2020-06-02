181878980b470249a9fca9d14c4f7051b90799b4
Hans <hansmandersingh1998@gmail.com>
2020-06-02 11:23:07 -0500

    Adds ability to add items to the cart but total and delete needs some work

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React from 'react';
-import './App.css';
-import Item from './Item';
+import React from "react";
+import "./App.css";
+import Item from "./Item";
+import Cart from "./Cart";
 
 export default class App extends React.Component {
   state = {
     allItems: [],
-    cartItems: []
-  }
+    cartItems: [],
+  };
 
   componentDidMount = () => {
     fetch(` https://5ed0108416017c00165e327c.mockapi.io/api/v1/items`)
-      .then(data => data.json())
-      .then(json => {
-        this.setState({allItems: json});
-      })
-  }
+      .then((data) => data.json())
+      .then((json) => {
+        this.setState({ allItems: json });
+      });
+  };
 
   addToCart = (item) => {
-    this.setState(prevState => ({
-      cartItems: [...prevState.cartItems, item]
-    }))
-  }
+    this.setState((prevState) => ({
+      cartItems: [...prevState.cartItems, item],
+    }));
+  };
 
   render() {
     return (
       <>
-        <header><h1>mouseHeav3an</h1></header>
-          <main>
+        <header>
+          <h1>mouseHeav3an</h1>
+        </header>
+        <main>
           <ul className="items">
-            {this.state.allItems.map(item => (
-              <Item key={item.id} item={item} addToCart={this.addToCart}/>
+            {this.state.allItems.map((item) => (
+              <Item key={item.id} item={item} addToCart={this.addToCart} />
             ))}
           </ul>
-          </main>
+          <aside className="cart">
+            <h2>Your Cart</h2>
+            <ul>
+              {this.state.cartItems.map(item => (
+                <Cart key={item.id} item={item}/>
+              ))}
+            </ul>
+            <div className="total">Total: $309.97</div>
+          </aside>
+        </main>
       </>
-    )
+    );
   }
 }
--- src/Cart.js
+++ src/Cart.js
@@ -1,2 +1,11 @@
-import React from 'react';
+import React from "react";
 
+export default function Cart(props) {
+  return (
+    <li className="cart-item">
+      <div className="name">{props.item.name}</div>
+      <button>X</button>
+      <div className="price">@ ${props.item.price}</div>
+    </li>
+  );
+}
