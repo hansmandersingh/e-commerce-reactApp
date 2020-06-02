0edb23e4543679a3fe96397c2fa8445124d434a0
Hans <hansmandersingh1998@gmail.com>
2020-06-02 11:07:11 -0500

    Adds items to the page and populates the results

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import Item from './Item';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    cart: [],
+  }
+
+  componentDidMount = () => {
+    fetch(` https://5ed0108416017c00165e327c.mockapi.io/api/v1/items`)
+      .then(data => data.json())
+      .then(json => {
+        this.setState({cart: json});
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <header><h1>mouseHeav3an</h1></header>
+          <main>
+          <ul className="items">
+            {this.state.cart.map(item => (
+              <Item key={item.id} item={item}/>
+            ))}
+          </ul>
+          </main>
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/Item.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Item(props) {
+  return (
+    <li className="item">
+      <img src={props.item.imageURL} alt="img"/>
+      <h2>{props.item.name}</h2>
+      <div className="price">$159.99</div>
+      <div className="description">
+        {props.item.description}
+      </div>
+      <button>Add to Cart</button>
+    </li>
+  );
+}
